f3193d16dcea5cb76cdcc52f3c167a3bbf318a36
Baye Manga <bayemanga16@gmail.com>
2025-01-28 14:32:20 +0300

    reconfigured cors

--- server/src/index.mjs
+++ server/src/index.mjs
@@ -17,7 +17,7 @@ const __dirname = path.resolve();
 connect_db();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["https://baye-chat.onrender.com", "http://localhost:5173"],
     credentials: true,
   })
 );
